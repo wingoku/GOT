66883558e651f9bbe49ad29465cfb8668ad10b1a
Umer Farooq <umer.farooq@picavi.com>
2021-02-10 16:57:14 +0100

    Added: utility methods

---
+++ app/src/main/java/com/wingoku/gameofthrones/utils/Utils.java
@@ -0,0 +1,24 @@
+package com.wingoku.gameofthrones.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Utils {
+    /**
+     * Check if network is available
+     * @param con Context of the activity/fragment/application
+     * @return true/false for network availability and offline respectively
+     */
+    public static boolean isNetworkAvailable(Context con) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) con.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    public static String getFlagImageUrl(String region) {
+        return Constants.regionImageUrlMap.containsKey(region) ?
+                Constants.regionImageUrlMap.get(region) :
+                "";
+    }
+}
